e72e2e395f3197a26979a0d330466b89eaf8c431
Harshit Chandasana <71175956+hchandasana@users.noreply.github.com>
2021-01-09 16:29:28 +0530

    Updated content.js to bold specific lines from the paragraph

--- content.js
+++ content.js
@@ -1,11 +1,18 @@
 chrome.runtime.onMessage.addListener((request) => {
   if (request.message === 'clicked_browser_action') {
     let dsr;
+    let data = ['Daily', 'Project', 'Completed', 'Tomorrow\'s'];
 
     dsr = window.getSelection().toString();
     dsr = dsr.split('\n').splice(0, dsr.split('\n').length - 2).join('\n')
     dsr = dsr.replace(/^\s*$(?:\r\n?|\n)/gm, '');
     dsr = dsr.split('"').join('');
+    
+    data = data.map(item => dsr.split('\n').filter(item1 => item1.includes(item))).flat();
+    data = [... new Set(data)];
+    data.map(item => {       
+      dsr = dsr.split(item).join(`*${item}*`);
+    });
 
     var $temp = $('<textarea>');
     var brRegex = /<br\s*[\/]?>/gi;
